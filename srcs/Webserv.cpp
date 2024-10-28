c5c88cc1dc15ad1ab9e9799d6d26cfc16eb9c209
Thomas Lam <thomas.lam0411@gmail.com>
2024-10-28 12:31:16 +0100

    fixed buffer error

--- srcs/Webserv.cpp
+++ srcs/Webserv.cpp
@@ -1,156 +1,5 @@
 #include "../headers/Webserv.hpp"
 
-Webserv::Webserv(std::string config)
-{
-	std::ifstream input(config.c_str());
-	if (!input.is_open()) {
-		std::cerr << "Can't open " << config << std::endl;
-		exit (-1);
-	}
-
-	parseConfigFile(input);
-	serversInit();
-}
-
-void Webserv::parseConfigFile(std::ifstream& input)
-{
-	std::vector<std::string> configVec;
-	std::string line;
-
-	    while (std::getline(input, line)) {
-        std::string newConfig;
-        if (trim(line) == "server {") {
-            while (std::getline(input, line) && trim(line) != "}") {
-                newConfig += line + "\n";
-            }
-        }
-        if (!newConfig.empty())
-        configVec.push_back(newConfig);
-    }
-
-	for (size_t i = 0; i < configVec.size(); i++) {
-		Server server(configVec[i]);
-		_servers.push_back(server);
-	}
-
-	if (_servers.size() < 1) {
-		std::cerr << "Error, invalid config file.\n";
-		exit (1);
-	}
-}
-
-void Webserv::serversInit()
-{
-	for (size_t i = 0; i < _servers.size(); i++) {
-		_servers[i].initAll();
-	}
-}
-
-void Webserv::run()
-{
-	epollInit();
-	eventLoop();
-}
-
-void Webserv::epollInit()
-{
-	_epoll_fd = epoll_create(1);
-	if (_epoll_fd < 0) {
-		std::cerr << "epoll created failed\n";
-		exit (-1);
-	}
-
-	_events.resize(_servers.size());
-
-	for (size_t i = 0; i < _servers.size(); i++) {
-		_servers[i].setEvent(_events[i]);
-		_servers[i].epollInit(_epoll_fd);
-	}
-}
-
-void Webserv::eventLoop() {
-	int maxEvents = 10;// originally 10
-	_events.resize(maxEvents);
-	while (true) {
-		int fd_number = epoll_wait(_epoll_fd, _events.data(), maxEvents, _servers[0].getTimeout());
-		if (fd_number < 0) {
-			std::cerr << "epoll_wait failed\n";
-			exit (-1);
-		}
-
-		for (int i = 0; i < fd_number; i++) {
-			int event_fd = _events[i].data.fd;
-
-			bool isServerSocket = false;
-			for (size_t j = 0; j < _servers.size(); j++) {
-				if (_servers[j].getServerFd() == event_fd) {
-					_servers[j].handleNewConnection();
-					isServerSocket = true;
-					break;
-				}
-			}
-			if (!isServerSocket) {
-				if (_events[i].events & EPOLLIN) {
-					Request* request = findAppropriateRequest(event_fd);
-					handleClientRequest(event_fd, *request);
-				}
-				if (_events[i].events & EPOLLOUT) {
-					Request* request = findAppropriateRequestToWrite(event_fd);
-					if (request != NULL /*&& request->isRequestComplete() && !request._buffer.empty()*/) {
-						handleClientWrite(event_fd, *request);
-						removeRequest(event_fd);
-					}
-				}
-			}
-		}
-	}
-}
-
-Request* Webserv::findAppropriateRequest(int event_fd)
-{
-	for (size_t i = 0; i < _requests.size(); i++) {
-		if (_requests[i].getClientFD() == event_fd) {
-			// std::cout << "FOUND EXISTING REQUEST TO READ\n";
-			return &_requests[i];
-		}
-	}
-	// std::cout << "CREATING NEW REQUEST\n";
-    _requests.push_back(Request());
-    _requests.back().setClientFD(event_fd);
-    return &_requests.back();
-}
-
-Request* Webserv::findAppropriateRequestToWrite(int event_fd)
-{
-	for (size_t i = 0; i < _requests.size(); i++) {
-		if (_requests[i].getClientFD() == event_fd) {
-			// std::cout << "FOUND EXISTING REQUEST TO WRITE\n";
-			return &_requests[i];
-		}
-	}
-	return NULL;
-}
-
-void Webserv::handleClientWrite(int event_fd, Request& request)
-{
-	if (request.isRequestComplete()) {
-		Response response(request);
-		response.handleRequest();
-		response.sendResponse(event_fd);
-	}
-}
-
-void Webserv::removeRequest(int event_fd)
-{
-    for (std::vector<Request>::iterator it = _requests.begin(); it != _requests.end(); ++it) {
-        if (it->getClientFD() == event_fd) {
-            _requests.erase(it);
-			// std::cout << "REMOVED REQUEST\n";
-            break;
-        }
-    }
-}
-
 int	checkUriSize(std::string buff)
 {
 	size_t	pos;
@@ -329,6 +178,163 @@ int	checkContentLengthSize(std::string buff)
 	return (0);
 }
 
+Webserv::Webserv(std::string config)
+{
+	std::ifstream input(config.c_str());
+	if (!input.is_open()) {
+		std::cerr << "Can't open " << config << std::endl;
+		exit (-1);
+	}
+
+	parseConfigFile(input);
+	serversInit();
+}
+
+void Webserv::parseConfigFile(std::ifstream& input)
+{
+	std::vector<std::string> configVec;
+	std::string line;
+
+	    while (std::getline(input, line)) {
+        std::string newConfig;
+        if (trim(line) == "server {") {
+            while (std::getline(input, line) && trim(line) != "}") {
+                newConfig += line + "\n";
+            }
+        }
+        if (!newConfig.empty())
+        configVec.push_back(newConfig);
+    }
+
+	for (size_t i = 0; i < configVec.size(); i++) {
+		Server server(configVec[i]);
+		_servers.push_back(server);
+	}
+
+	if (_servers.size() < 1) {
+		std::cerr << "Error, invalid config file.\n";
+		exit (1);
+	}
+}
+
+void Webserv::serversInit()
+{
+	for (size_t i = 0; i < _servers.size(); i++) {
+		_servers[i].initAll();
+	}
+}
+
+void Webserv::run()
+{
+	epollInit();
+	eventLoop();
+}
+
+void Webserv::epollInit()
+{
+	_epoll_fd = epoll_create(1);
+	if (_epoll_fd < 0) {
+		std::cerr << "epoll created failed\n";
+		exit (-1);
+	}
+
+	_events.resize(_servers.size());
+
+	for (size_t i = 0; i < _servers.size(); i++) {
+		_servers[i].setEvent(_events[i]);
+		_servers[i].epollInit(_epoll_fd);
+	}
+}
+
+void Webserv::eventLoop() {
+	int maxEvents = 10;// originally 10
+	_events.resize(maxEvents);
+	while (true) {
+		int fd_number = epoll_wait(_epoll_fd, _events.data(), maxEvents, _servers[0].getTimeout());
+		if (fd_number < 0) {
+			std::cerr << "epoll_wait failed\n";
+			exit (-1);
+		}
+
+		for (int i = 0; i < fd_number; i++) {
+			int event_fd = _events[i].data.fd;
+
+			bool isServerSocket = false;
+			for (size_t j = 0; j < _servers.size(); j++) {
+				if (_servers[j].getServerFd() == event_fd) {
+					_servers[j].handleNewConnection();
+					isServerSocket = true;
+					break;
+				}
+			}
+			// std::cout << "_requests size in loop == " << _requests.size() << std::endl;
+			if (!isServerSocket) {
+				if (_events[i].events & EPOLLIN) {
+					Request* request = findAppropriateRequest(event_fd);
+					handleClientRequest(event_fd, *request);
+				}
+				if (_events[i].events & EPOLLOUT) {
+					Request* request = findAppropriateRequestToWrite(event_fd);
+					if (request != NULL && request->isRequestComplete() && !request->_buffer.empty()) {
+						handleClientWrite(event_fd, *request);
+						removeRequest(event_fd);
+					}
+				}
+			}
+		}
+	}
+}
+
+Request* Webserv::findAppropriateRequest(int event_fd)
+{
+	// std::cout << "EVENT FD = " << event_fd << std::endl;
+	// std::cout << "_requests.size() before == " << _requests.size() << std::endl;
+
+	for (size_t i = 0; i < _requests.size(); i++) {
+		// std::cout << "request[" << i << "]._client_fd = " << _requests[i].getClientFD() << std::endl;
+		if (_requests[i].getClientFD() == event_fd) {
+			// std::cout << "FOUND EXISTING REQUEST TO READ\n";
+			return &_requests[i];
+		}
+	}
+	std::cout << "CREATING NEW REQUEST\n";
+    _requests.push_back(Request());
+    _requests.back().setClientFD(event_fd);
+	// std::cout << "_requests.size() after == " << _requests.size() << std::endl;
+    return &_requests.back();
+}
+
+Request* Webserv::findAppropriateRequestToWrite(int event_fd)
+{
+	for (size_t i = 0; i < _requests.size(); i++) {
+		if (_requests[i].getClientFD() == event_fd) {
+			// std::cout << "FOUND EXISTING REQUEST TO WRITE\n";
+			return &_requests[i];
+		}
+	}
+	return NULL;
+}
+
+void Webserv::handleClientWrite(int event_fd, Request& request)
+{
+	if (request.isRequestComplete()) {
+		Response response(request);
+		response.handleRequest();
+		response.sendResponse(event_fd);
+	}
+}
+
+void Webserv::removeRequest(int event_fd)
+{
+    for (std::vector<Request>::iterator it = _requests.begin(); it != _requests.end(); ++it) {
+        if (it->getClientFD() == event_fd) {
+            _requests.erase(it);
+			std::cout << "REMOVED REQUEST\n";
+            break;
+        }
+    }
+}
+
 int	checkAllSize(Request request)
 {
 	if (checkUriSize(request._buffer) == 414)
@@ -353,9 +359,9 @@ void Webserv::handleClientRequest(int client_fd, Request& request)
         return;
     }
 	// buffer[bytes] = 0;
-	// std::cout << "BUFFER = " << buffer << "\n\n";
+	std::cout << "BUFFER = " << buffer << "\n\n";
 	request._buffer += std::string(buffer);
-	// std::cout << "INCOMPLETE BUFFER = " << request._buffer << "\n\n";
+	std::cout << "INCOMPLETE BUFFER = " << request._buffer << "\n\n";
 
 	request.setStatusCode(checkAllSize(request));
 	if (request.getStatusCode() != 0)
